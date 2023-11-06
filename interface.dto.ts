5dc0958e1436baa0eb001bec755f778ba1a7a12a
kien.pham2 <kien.pham2@vndirect.com.vn>
2023-11-06 16:35:11 +0700

    init lib

---
+++ interface.dto.ts
@@ -0,0 +1,13 @@
+export interface LoadingProps {
+  size?: string;
+  color?: string;
+}
+
+export interface SpinnerOriginalProps {
+  className?: string;
+  color?: string;
+}
+
+export interface GlobalSpinnerProps {
+  size?: string;
+}
