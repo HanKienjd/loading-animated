04b3d6f2092c1703da70eb3d8bcd295bde833a9c
Hankienjd <kienp544@gmail.com>
2024-01-21 17:25:28 +0700

    fix build code

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.tsx'),
-      name: 'my-library',
+      name: 'loading-animated',
 
       fileName: 'index',
     },
